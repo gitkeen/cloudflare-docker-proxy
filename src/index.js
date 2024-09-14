6cf59b11e9348f2ad88a90114a65f6a60a505c75
gitkeen <55470144+gitkeen@users.noreply.github.com>
2024-09-14 18:24:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.eth.vg": dockerHub,
+  "quay.eth.vg": "https://quay.io",
+  "gcr.eth.vg": "https://gcr.io",
+  "k8s-gcr.eth.vg": "https://k8s.gcr.io",
+  "k8s.eth.vg": "https://registry.k8s.io",
+  "ghcr.eth.vg": "https://ghcr.io",
+  "cloudsmith.eth.vg": "https://docker.cloudsmith.io",
+  "ecr.eth.vg": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.eth.vg": dockerHub,
 };
 
 function routeByHosts(host) {
